8c87c7838bea022fb738cbfd70ff4f86d4b677de
MichelleAaa <99776009+MichelleAaa@users.noreply.github.com>
2022-08-04 13:49:10 -0400

    Added Class Notes

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const campsiteRouter = express.Router();
 
+//We are setting it up in server.js for 'campsites', so here we only use /
 campsiteRouter.route('/')
+//Note that .all, .get, .post, etc. have been chained onto the campsiteRouter.route method.
 .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+    res.setHeader('Content-Type', 'text/plain'); //plain means plain text.
+    next(); //next function passes control to the next routing method after this point. Otherwise, we would just stop here and not move forward.
 })
+//After the express server handles the code for the app.all method…. If the request is a post request, it would go to the app.all method, skipping the app.get method, and going to the app.post method. Only .all is done for all.
 .get((req, res) => {
     res.end('Will send all the campsites to you');
 })
 .post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`); //express.json in the server.js file had already parsed the data with JSON, so we are accessing it with req.body.name and req.body.description here.
 })
 .put((req, res) => {
-    res.statusCode = 403;
+    res.statusCode = 403; //403 is when an operation is not supported.
     res.end('PUT operation not supported on /campsites');
 })
-.delete((req, res) => {
+.delete((req, res) => {//This is normally a dangerous operation, so if it's set up for real we would make sure ordinary users are not allowed to do it.
     res.end('Deleting all campsites');
-});
+}); //Note that this is the only one with the ;, as it’s the end of the chain. We have a single statement handling all routing for /campsites.
 
-campsiteRouter.route('/:campsiteId')
+campsiteRouter.route('/:campsiteId')//The route parameter at the end will allow us to store whatever the client sends as part of the path, called campsiteId. 
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); // Note that we don’t need to list the file path, only express.
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
@@ -9,15 +9,18 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json()); //when the server receives requests with JSON, this middleware function will parse the data into objects, so that we can use that data in JS.
 
-app.use('/campsites', campsiteRouter);
+app.use('/campsites', campsiteRouter); //this is where we set the name for /campsites, and we don’t specify it in campsiteRouter.js. (we instead use '/' there). The rest of the routes below work the same. 
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 
+// After npm start-ing in the terminal, this allows us to see the static websites located in the public folder (Express will serve them.):
 app.use(express.static(__dirname + '/public'));
 
+// If line 19 were commented out, then localhost:3000/ would still load, but it would show the content below:
 app.use((req, res) => {
+    //request object, response object, are the parameters.
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
